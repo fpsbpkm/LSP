d27228f2f6daacc7d085d9dc5f71b0423f102091
mhrwh <i072ff@yamaguchi-u.ac.jp>
2021-10-06 15:19:32 +0900

    getWordRangeAtPosition修正

--- lsp-sample/server/src/server.ts
+++ lsp-sample/server/src/server.ts
@@ -243,24 +243,13 @@ connection.onHover(
 
 		let found;
 		let wordRange: Range | undefined;
-		if (/(\w+:def\s+\d+|\w+:\s*\d+|\w+:sch\s+\d+)/g.test(text)){
-			let result;
-			const p = /(\w+:def\s+\d+|\w+:\s*\d+|\w+:sch\s+\d+)/g;
-			while (result = p.exec(text)) {
-				if(position.character < p.lastIndex){
-					wordRange = {
-						"start": { "line": position.line, "character": result.index },
-			            "end": { "line": position.line, "character": p.lastIndex }
-					}
-					break;
-				}
-			}			
-		}
-		else if (found = /(by\s+(\w+(,|\s|:)*)+|from\s+\w+(:sch\s+\d+)*\((\w+,*)+\))/g.exec(text)){
+
+		if (found = /(by\s+(\w+(,|\s|:)*)+|from\s+\w+(:sch\s+\d+)*\((\w+,*)+\))/g.exec(text)){
 			const index = found.index;
 			let result;
-			const p = /\w+/g;
+			const p = /\w+:def\s+\d+|\w+:\s*\d+|\w+:sch\s+\d+|\w+/g;
 			while (result = p.exec(found[0])) {
+				
 				if(position.character < index+p.lastIndex){
 					wordRange = {
 						"start": { "line": position.line, "character": index+result.index },
